b81bfb9a470ffabe4784c1b68bc17996b65c0914
Álvaro Pérez García <alvaro_093@hotmail.com>
2021-04-14 10:34:56 +0200

    Added serverless resources

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,6 @@
 import app from "./app";
+import serverless from "serverless-http";
 import { PORT } from "./constants";
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`)); 
+module.exports.handler = serverless(app);
